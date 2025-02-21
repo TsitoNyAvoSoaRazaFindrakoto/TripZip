401bd0620a2ca33274ec67ae3c9082fc776d65e2
Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
2025-02-21 10:45:28 +0300

    Update ReservationController.java
    frame work supports exception throwing

--- src/controllers/ReservationController.java
+++ src/controllers/ReservationController.java
@@ -7,16 +7,19 @@ import mg.itu.prom16.annotations.parameter.Param;
 import mg.itu.prom16.annotations.parameter.ParamObject;
 import mg.itu.prom16.annotations.request.Controller;
 import mg.itu.prom16.annotations.request.RequestMapping;
+import mg.itu.prom16.annotations.validation.Fallback;
 import mg.itu.prom16.embed.EmbedSession;
 import mg.itu.prom16.types.returnType.ModelAndView;
+import models.exception.ReservationValidationException;
 import models.reservation.Reservation;
 import models.vol.DetailsPlace;
 
 @Controller
 public class ReservationController {
 
+	@Fallback(method = "GET", verb = "/TripZip/reservation")
 	@RequestMapping(path = "/TripZip/reservation", method = "POST")
-	public ModelAndView saveReservation(@ParamObject Reservation reservation) {
+	public ModelAndView saveReservation(@ParamObject Reservation reservation) throws Exception {
 		ModelAndView mv = new ModelAndView("/TripZip/client");
 		try (Connection c = Connect.getConnection()) {
 			reservation.validateReservation();
@@ -24,22 +27,26 @@ public class ReservationController {
 			reservation.save(c);
 			mv.setAttribute("success", "Reservation effectuée avec succès");
 			c.commit();
-		} catch (Exception e) {
+		} catch (ReservationValidationException e) {
 			mv.setAttribute("error", e.getMessage());
-			mv.setView("/TripZip/client");
+			mv.setView("/TripZip/reservation");
+			return mv;
+		} catch (Exception e) {
+			throw e;
 		}
 		return mv;
 	}
 
 	@RequestMapping(path = "/TripZip/reservation")
-	public ModelAndView toReservation(EmbedSession embedSession, @Param int idSiegeVol) {
+	public ModelAndView toReservation(EmbedSession embedSession, @Param int idSiegeVol) throws Exception {
 		ModelAndView mv = new ModelAndView(true, "views/frontend/reservation.jsp");
 		DetailsPlace detailsPlace = DetailsPlace.getByIdSiegeVol(idSiegeVol);
-		try(Connection c = Connect.getConnection()) {
+		try (Connection c = Connect.getConnection()) {
 			detailsPlace.getVilleArrivee(c);
 			detailsPlace.getVilleDepart(c);
 			detailsPlace.getAvion(c);
 		} catch (Exception e) {
+			throw e;
 		}
 		mv.setAttribute("vol", detailsPlace);
 		return mv;
