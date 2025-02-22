dab9abd2e8fe5480c64208cbacea1e9b8ee9a6cc
Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
2025-02-22 04:07:45 +0300

    update volController
    added method  to see avilable flights

--- src/controllers/ClientController.java
+++ src/controllers/ClientController.java
@@ -10,33 +10,9 @@ import mg.itu.prom16.embed.EmbedSession;
 import mg.itu.prom16.types.returnType.ModelAndView;
 import models.Utilisateur;
 import models.reservation.Reservation;
-import models.vol.DetailsPlace;
 
 @Controller
 public class ClientController {
-	/*
-	 * public ModelAndView toClient(EmbedSession embedSession) throws Exception {
-	 * ModelAndView mv = new ModelAndView(true, "views/frontend/index.jsp");
-	 * try (Connection c = Connect.getConnection()) {
-	 * List<DetailsPlace> detailsPlace = DetailsPlace.getAllDispo(c, 1, 10);
-	 * detailsPlace.stream().forEach(d -> {
-	 * try {
-	 * d.getVilleArrivee(c);
-	 * d.getVilleDepart(c);
-	 * d.getAvion(c);
-	 * } catch (Exception e) {
-	 * mv.setAttribute("error", e.getMessage());
-	 * }
-	 * });
-	 * mv.setAttribute("count", Connect.getCount(c, "details_place"));
-	 * mv.setAttribute("vols", detailsPlace);
-	 * } catch (Exception e) {
-	 * throw e;
-	 * }
-	 * 
-	 * return mv;
-	 * }
-	 */
 	@RequestMapping(path = "/TripZip/client")
 	public ModelAndView toClient(EmbedSession embedSession) throws Exception {
 		ModelAndView mv = new ModelAndView("views/frontend/index.jsp");
@@ -53,7 +29,7 @@ public class ClientController {
 					mv.setView("views/error.jsp");
 					mv.setAttribute("error", e);
 					System.out.println(e.getMessage());
-					return ;
+					return;
 				}
 			});
 			mv.setAttribute("reservations", reservations);
--- src/controllers/VolController.java
+++ src/controllers/VolController.java
@@ -1,8 +1,36 @@
 package controllers;
 
+import java.sql.Connection;
+import java.util.List;
+
+import database.Connect;
 import mg.itu.prom16.annotations.request.Controller;
+import mg.itu.prom16.embed.EmbedSession;
 import mg.itu.prom16.types.returnType.ModelAndView;
+import models.vol.DetailsPlace;
 
 @Controller
 public class VolController {
+
+	public ModelAndView toClient(EmbedSession embedSession) throws Exception {
+		ModelAndView mv = new ModelAndView(true, "views/frontend/index.jsp");
+		try (Connection c = Connect.getConnection()) {
+			List<DetailsPlace> detailsPlace = DetailsPlace.getAllDispo(c, 1, 10);
+			detailsPlace.stream().forEach(d -> {
+				try {
+					d.getVilleArrivee(c);
+					d.getVilleDepart(c);
+					d.getAvion(c);
+				} catch (Exception e) {
+					mv.setAttribute("error", e.getMessage());
+				}
+			});
+			mv.setAttribute("count", Connect.getCount(c, "details_place"));
+			mv.setAttribute("vols", detailsPlace);
+		} catch (Exception e) {
+			throw e;
+		}
+
+		return mv;
+	}
 }
