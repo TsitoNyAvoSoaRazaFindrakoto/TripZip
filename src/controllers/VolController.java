a81e65470d330c563d672252419466ca3364961d
Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
2025-02-22 05:26:59 +0300

    Update VolController.java

--- src/controllers/VolController.java
+++ src/controllers/VolController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import database.Connect;
 import mg.itu.prom16.annotations.request.Controller;
+import mg.itu.prom16.annotations.request.RequestMapping;
 import mg.itu.prom16.embed.EmbedSession;
 import mg.itu.prom16.types.returnType.ModelAndView;
 import models.vol.DetailsPlace;
@@ -12,6 +13,7 @@ import models.vol.DetailsPlace;
 @Controller
 public class VolController {
 
+	@RequestMapping(path = "/TripZip/vols")
 	public ModelAndView toClient(EmbedSession embedSession) throws Exception {
 		ModelAndView mv = new ModelAndView(true, "views/frontend/index.jsp");
 		try (Connection c = Connect.getConnection()) {
@@ -22,7 +24,10 @@ public class VolController {
 					d.getVilleDepart(c);
 					d.getAvion(c);
 				} catch (Exception e) {
-					mv.setAttribute("error", e.getMessage());
+					mv.setView("views/error.jsp");
+					mv.setAttribute("error", e);
+					System.out.println(e.getMessage());
+					return;
 				}
 			});
 			mv.setAttribute("count", Connect.getCount(c, "details_place"));
