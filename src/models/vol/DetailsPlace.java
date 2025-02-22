a1f6bbb720a370e6908c54f7dbc703fece6b4db4
Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
2025-02-22 19:07:05 +0300

    file optim

--- src/models/vol/DetailsPlace.java
+++ src/models/vol/DetailsPlace.java
@@ -111,6 +111,7 @@ public class DetailsPlace extends Vol {
 			inside = true;
 		}
 		List<DetailsPlace> list = new ArrayList<>();
+		System.out.println("SELECT * FROM details_place LIMIT ? OFFSET ?");
 		String sql = "SELECT * FROM details_place LIMIT ? OFFSET ?";
 		try (
 				PreparedStatement pstmt = conn.prepareStatement(sql)) {
