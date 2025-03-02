69a0d42af5d48f27223fef2e6b02e9922651b0d4
Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
2025-03-02 22:44:20 +0300

    Update VolController.java

--- src/controllers/VolController.java
+++ src/controllers/VolController.java
@@ -10,6 +10,7 @@ import mg.itu.prom16.annotations.request.RequestMapping;
 import mg.itu.prom16.embed.EmbedSession;
 import mg.itu.prom16.types.returnType.ModelAndView;
 import models.vol.DetailsPlace;
+import models.vol.FormDTO;
 
 @Controller
 public class VolController {
@@ -18,7 +19,7 @@ public class VolController {
 	public ModelAndView toClient(EmbedSession embedSession, @Param Integer page) throws Exception {
 		ModelAndView mv = new ModelAndView("/views/frontend/index.jsp");
 		try (Connection c = Connect.getConnection()) {
-			List<DetailsPlace> detailsPlace = DetailsPlace.getAll(c, page, 8,false);
+			List<DetailsPlace> detailsPlace = DetailsPlace.getAll(c, page, 8, false);
 			detailsPlace.stream().forEach(d -> {
 				try {
 					d.getData(c);
@@ -29,8 +30,33 @@ public class VolController {
 					return;
 				}
 			});
-			mv.setAttribute("count", Connect.getCount(c, "details_place"));
+			mv.setAttribute("count", Connect.getCount(c, "vols_details"));
 			mv.setAttribute("page", page == null ? 1 : page);
+			mv.setAttribute("vols", detailsPlace);
+		} catch (Exception e) {
+			throw e;
+		}
+
+		return mv;
+	}
+
+	@RequestMapping(path = "/TripZip/vols/find")
+	public ModelAndView multiCriteraSearch(EmbedSession embedSession,FormDTO formDTO) throws Exception {
+		ModelAndView mv = new ModelAndView("/views/frontend/index.jsp");
+		try (Connection c = Connect.getConnection()) {
+			List<DetailsPlace> detailsPlace = DetailsPlace.getByCriteria(c,formDTO);
+			detailsPlace.stream().forEach(d -> {
+				try {
+					d.getData(c);
+				} catch (Exception e) {
+					mv.setView("/views/error.jsp");
+					mv.setAttribute("error", e);
+					System.out.println(e.getMessage());
+					return;
+				}
+			});
+			mv.setAttribute("count", -1);
+			mv.setAttribute("page", 1);
 			mv.setAttribute("vols", detailsPlace);
 		} catch (Exception e) {
 			throw e;
