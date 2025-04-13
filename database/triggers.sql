fca35704342a0c7351e7464df370516af1658fb3
Razafindrakoto Tsito Ny Avo Soa <153315298+TsitoNyAvoSoaRazaFindrakoto@users.noreply.github.com>
2025-04-13 21:47:23 +0300

    Update triggers.sql

--- database/triggers.sql
+++ database/triggers.sql
@@ -1,12 +1,20 @@
 CREATE OR REPLACE FUNCTION update_vol_dates()
 RETURNS TRIGGER AS $$
+DECLARE
+    res_time time;
+    ann_time time;
 BEGIN
-	-- Subtract reservation duration from date_vol
-		NEW.reservation := NEW.date_vol - (SELECT duree FROM Config_reservation WHERE libelle = 'reservation')::interval;
-
-	-- Subtract cancellation duration from date_vol
-		NEW.annulation := NEW.date_vol - (SELECT duree FROM Config_reservation WHERE libelle = 'annulation')::interval;
-	RETURN NEW;
+    -- Get the configured times
+    SELECT value::time INTO res_time FROM rule_config WHERE id = 'reservation';
+    SELECT value::time INTO ann_time FROM rule_config WHERE id = 'annulation';
+    
+    -- Subtract reservation time from flight time
+    NEW.reservation := NEW.date_vol - (res_time::interval);
+    
+    -- Subtract annulation time from flight time
+    NEW.annulation := NEW.date_vol - (ann_time::interval);
+    
+    RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
@@ -17,19 +25,26 @@ EXECUTE FUNCTION update_vol_dates();
 
 CREATE OR REPLACE FUNCTION update_vol_dates_on_update()
 RETURNS TRIGGER AS $$
+DECLARE
+    res_time time;
+    ann_time time;
 BEGIN
-	IF NEW.date_vol != OLD.date_vol IS NULL THEN
-	-- Subtract reservation duration from date_vol
-		NEW.reservation := NEW.date_vol - (SELECT duree FROM Config_reservation WHERE libelle = 'reservation')::interval;
-
-	-- Subtract cancellation duration from date_vol
-		NEW.annulation := NEW.date_vol - (SELECT duree FROM Config_reservation WHERE libelle = 'annulation')::interval;
-	end if;
-	RETURN NEW;
+    IF NEW.date_vol != OLD.date_vol THEN
+        -- Get the configured times
+        SELECT value::time INTO res_time FROM rule_config WHERE id = 'reservation';
+        SELECT value::time INTO ann_time FROM rule_config WHERE id = 'annulation';
+        
+        -- Subtract reservation time from flight time
+        NEW.reservation := NEW.date_vol - (res_time::interval);
+        
+        -- Subtract annulation time from flight time
+        NEW.annulation := NEW.date_vol - (ann_time::interval);
+    END IF;
+    RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER update_vol_dates_on_update
 BEFORE UPDATE ON Vol
 FOR EACH ROW
-EXECUTE FUNCTION update_vol_dates();
+EXECUTE FUNCTION update_vol_dates_on_update();
