4ac50a6c95ac3e70ab950f10e9330f6c6e48774e
Razafindrakoto Tsito Ny Avo Soa <153315298+TsitoNyAvoSoaRazaFindrakoto@users.noreply.github.com>
2025-08-17 15:09:48 +0300

    Update drop.sql

--- database/drop.sql
+++ database/drop.sql
@@ -6,15 +6,15 @@ DROP TRIGGER IF EXISTS vol_dates_before_update ON Vol;
 DROP FUNCTION IF EXISTS update_vol_dates();
 
 -- Drop views
-DROP VIEW IF EXISTS details_place;
+DROP VIEW IF EXISTS details_place cascade;
 
 -- Drop tables in reverse order of creation to avoid foreign key constraints
-DROP TABLE IF EXISTS rule_config;
-DROP TABLE IF EXISTS Reservation;
-DROP TABLE IF EXISTS Sieges_Avions;
-DROP TABLE IF EXISTS Siege_Vol;
-DROP TABLE IF EXISTS Vol;
-DROP TABLE IF EXISTS Ville;
-DROP TABLE IF EXISTS Siege;
-DROP TABLE IF EXISTS Utilisateur;
-DROP TABLE IF EXISTS Avion;
+DROP TABLE IF EXISTS rule_config cascade;
+DROP TABLE IF EXISTS Reservation cascade;
+DROP TABLE IF EXISTS Sieges_Avions cascade;
+DROP TABLE IF EXISTS Siege_Vol cascade;
+DROP TABLE IF EXISTS Vol cascade;
+DROP TABLE IF EXISTS Ville cascade;
+DROP TABLE IF EXISTS Siege cascade;
+DROP TABLE IF EXISTS Utilisateur cascade;
+DROP TABLE IF EXISTS Avion cascade;
